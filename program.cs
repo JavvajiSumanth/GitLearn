674362e6921a57b711b4fb7adc1ca566386c2c84
vk2098 <kapavivek98@gmail.com>
2024-03-16 18:48:17 -0500

    added commits

---
+++ program.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Hello
+{
+    int n1=10,n2=15;
+    static void Main()
+    {
+        n1=10;
+        n2=20;
+        Console.WriteLine(n1+n2);
+        //Console.ReadLine("123");
+    }
+
+    // kapa edited this in temp file commit1
+    // kapa edited this in temp file commit2
+    // kapa edited this in temp file commit3
+    // hello world
+    // kapa edited this in temp file 
+
+#1
+}
+
+//kapa
+
