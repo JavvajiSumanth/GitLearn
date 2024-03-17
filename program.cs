ff4addb29414625866275791c0d59270ecd92947
JavvajiSumanth <sumanthjavvaji99@gmail.com>
2024-03-16 19:56:30 -0500

    l

--- program.cs
+++ program.cs
@@ -9,6 +9,14 @@ class Hello
         n2=20;
         Console.WriteLine(n1+n2);
         //Console.ReadLine("123");
+                //Console.ReadLine("123");
+
+                        //Console.ReadLine("123");
+                                //Console.ReadLine("123");
+                                        //Console.ReadLine("123");
+                                                //Console.ReadLine("123");
+                                                        //Console.ReadLine("123");
+                                                        
     }
 
     // kapa edited this in temp file commit1
